df2ae468f0b0cf569ad5aac09d9203090a9943e3
Hunter Kam <hone648@gmail.com>
2020-02-20 16:22:34 -0800

    Complete

--- 2_greater_than_y.js
+++ 2_greater_than_y.js
@@ -2,9 +2,14 @@
 // Predcted output: greaterThanY([1,6,8,3,5,2], 4) should return 3
 
 function greaterThanY(arr, y){
-    // YOUR CODE HERE
+    var newarr=[];
+    for(i=0; i<=arr.length-1; i++){
+        if(arr[i]>y){
+        newarr.push(arr[i]);
+        }
+    }
+    return newarr
 }
-var result = greaterThanY([1,6,8,3,5,2], 4);
+var result = greaterThanY([5,6,7,8,9,10], 4);
 console.log(result);
-
 
--- 3_shift_left.js
+++ 3_shift_left.js
@@ -1,8 +1,13 @@
 // Given an array, shift all values one position to the left.  Change the final position to 0. Return the changed array
 // Predicted Output: shiftLeft([1,2,3,4,5]) should return [2,3,4,5,0];
 
-function shitftLeft(arr){
-    // YOUR CODE HERE
+function shiftLeft(arr){
+    var newarr=[];
+    for(i=1; i<arr.length; i++){
+        newarr.push(arr[i]);
+    }
+    newarr.push(0);
+    return newarr
 }
-var resultArr = shiftLeft([1,2,3,4,5]);
+var resultArr = shiftLeft(["hello","hi","yo","wazzup"]);
 console.log(resultArr);
